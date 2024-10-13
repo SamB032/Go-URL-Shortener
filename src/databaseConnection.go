e2e85c6b588465580a811c6fadd0ac251835136f
Sam <sam.boffey04@gmail.com>
2024-10-13 09:43:16 +0100

    Adds a database connection

---
+++ src/databaseConnection.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+  _ "github.com/lib/pq"
+  "github.com/joho/godotenv"
+
+  "database/sql"
+  "fmt"
+  "os"
+)
+
+type PostgresData struct {
+  host     string
+  port     string
+  user     string
+  password string
+  dbName   string
+}
+
+// TODO: Write to use docker-compose environment variables
+//Load the posgres data from environment variables and return it as a struct
+func getDatabaseInfo() PostgresData {
+  err := godotenv.Load("../.env")
+  // Check if the .env file was loaded correctly
+  if err != nil {
+    panic(err)
+  }
+
+  return PostgresData{
+    os.Getenv("POSTGRES_HOST"), "5432", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"),
+  }
+}
+
+func connectToDatabase() (string, *sql.DB) {
+  dbInfo := getDatabaseInfo()
+
+  psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
+    dbInfo.host, dbInfo.port, dbInfo.user, dbInfo.password, dbInfo.dbName)
+
+  //Initalise a connection to the database
+  db, err := sql.Open("postgres", psqlInfo)
+  if err != nil {
+    panic(err)
+  }
+  defer db.Close()
+
+  // Make a ping to the database to see if its alive
+  err = db.Ping()
+  if err != nil {
+    panic(err)
+  }
+  return "Successfully connected to the database", db
+}
