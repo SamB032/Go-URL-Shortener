4a99d1a57e4f19c5ab35dccc815644f86a4f5fa2
Sam <sam.boffey04@gmail.com>
2025-04-13 13:29:27 +0100

    Create Secrets for namespaces

---
+++ resources/secrets.tf
@@ -0,0 +1,39 @@
+resource "random_id" "username" {
+  byte_length = 8
+}
+
+# Generate a random password
+resource "random_password" "password" {
+  length  = 16
+  special = true
+  upper   = true
+  lower   = true
+}
+
+# Secrets for Database
+resource "kubernetes_secret" "database" {
+  metadata {
+    name      = "postgres-secret"
+    namespace = kubernetes_namespace.namespace["database"].metadata[0].name
+  }
+
+  data = {
+    POSTGRES_USER     = random_id.username.hex
+    POSTGRES_PASSWORD = random_password.password.result
+    POSTGRES_DB       = "shortkey"
+  }
+}
+
+# Secrets for App
+resource "kubernetes_secret" "url-app" {
+  metadata {
+    name      = "postgres-secret"
+    namespace = kubernetes_namespace.namespace["url-app"].metadata[0].name
+  }
+
+  data = {
+    POSTGRES_USER     = random_id.username.hex
+    POSTGRES_PASSWORD = random_password.password.result
+    POSTGRES_DB       = "shortkey"
+  }
+}
