d5d35ba5cbd732af7482e62ccc64e72142e21375
Sam <sam.boffey04@gmail.com>
2025-04-20 19:12:19 +0100

    Add prometheus and serviceMonitor for traefik

--- resources/helm.tf
+++ resources/helm.tf
@@ -1,8 +1,13 @@
+## Core app services
+
 resource "helm_release" "database" {
   name  = "postgres-database"
   chart = "../helm/postgres-database"
 
   namespace = kubernetes_namespace.namespace["database"].metadata[0].name
+  values = [
+    file("../helm/postgres-database/values.yaml")
+  ]
 }
 
 resource "helm_release" "url-app" {
@@ -10,11 +15,29 @@ resource "helm_release" "url-app" {
   chart = "../helm/app"
 
   namespace = kubernetes_namespace.namespace["url-app"].metadata[0].name
+  values = [
+    file("../helm/app/values.yaml")
+  ]
 }
 
 resource "helm_release" "traefik" {
-  name  = "traefik"
-  chart = "../helm/traefik-reverse-proxy/"
-
+  name       = "traefik"
+  repository = "https://helm.traefik.io/traefik"
+  chart      = "traefik"
+  version    = "35.0.1"
+  
   namespace = "default"
+  values = [
+    file("../helm/traefik-reverse-proxy/values.yaml")
+  ]
+}
+
+## Observability
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+
+  namespace  = "monitoring"
+  values = [file("../helm/prometheus/values.yaml")]
 }
