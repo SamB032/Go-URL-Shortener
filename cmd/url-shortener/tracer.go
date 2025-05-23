3b609d1f2f0bdea59090c6f0aead44815e2f5a55
Sam <sam.boffey04@gmail.com>
2025-05-23 22:18:51 +0100

    Go fmt

--- cmd/url-shortener/tracer.go
+++ cmd/url-shortener/tracer.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 )
